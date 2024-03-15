8da1f4df8bb931cccd16558f44461e7fad411e22
Sean Prior <111420945+SeannPr@users.noreply.github.com>
2024-03-15 08:54:57 +0100

    Update Projekt.cs

--- Projekt.cs
+++ Projekt.cs
@@ -1,1 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Figgle;
 
+namespace BBT
+{
+    internal class main
+    {
+        Console.WriteLine("test");
+        
+    }
+}
